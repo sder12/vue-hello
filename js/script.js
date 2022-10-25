8cf61bda56c034be8fa55aa52bd958d2cd18c788
sder12 <sara.cetto@gmail.com>
2022-10-25 15:11:54 +0200

    Insert Img and Text with data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue;
+
+createApp({
+    data: function () {
+        return {
+            userName: "Mario Rossi",
+            titleUnsplash: "Explore Unsplash photos",
+            classImg: "img-unsplash",
+            srcImg: "https://source.unsplash.com/random/900×700/?fruit",
+            altText: "Fruits img published by unsplash",
+        }
+    }
+}).mount("#app")
